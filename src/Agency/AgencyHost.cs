fded949f38c2a92b89610b094584da9538980c3e
Bram Fokke <bram@clocktimizer.com>
2021-01-25 21:42:48 +0100

    Render route map in unity

---
+++ src/Agency/AgencyHost.cs
@@ -0,0 +1,14 @@
+﻿using Agency.Network.RoadRunner;
+
+namespace Agency
+{
+    public class AgencyHost
+    {
+        public AgencyHost()
+        {
+            Map = RouteMap.LoadBinary(@"D:\Data\OpenFietsModel\Gemeentes\0344\RouteMap.bin");
+        }
+        
+        public RouteMap Map { get; }
+    }
+}
